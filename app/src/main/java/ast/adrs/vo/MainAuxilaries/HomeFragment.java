4a602646b58f86fb726ee1f0fa3ee59901e20834
HasnainRasool <hasnainr128@gmail.com>
2021-11-05 17:13:02 +0500

    just working on bar chart not done yet

--- app/src/main/java/ast/adrs/vo/MainAuxilaries/HomeFragment.java
+++ app/src/main/java/ast/adrs/vo/MainAuxilaries/HomeFragment.java
@@ -28,16 +28,16 @@ import ast.adrs.vo.IntroAuxilaries.WebServices.AppConstt;
 import ast.adrs.vo.Utils.IBadgeUpdateListener;
 
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment  {
     private IBadgeUpdateListener mBadgeUpdateListener;
     private PieChart pieChart;
-  //  private BarChart barChart;
+    //  private BarChart barChart;
     HorizontalBarChart barChart;
-    private PieChart  mBarChart_dieses_idr ;
-    private PieChart  mBarChart_sick_animal;
+    private PieChart mBarChart_dieses_idr;
+    private PieChart mBarChart_sick_animal;
     private List<Integer> lstPieValues;
     int InProgress, Mapped;
- //   Database database;
+    //   Database database;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,40 +48,46 @@ public class HomeFragment extends Fragment {
         init();
         bindViews(frg);
         showExampleGraph(frg);
-       // showBarChart(frg);
-        SetData(12,50);
+        // showBarChart(frg);
+         SetData(5,5);
         showIDR();
-        showSWSickAnimal();
+        // showSWSickAnimal();
         return frg;
+
+        /////
+
+
     }
 
     private void bindViews(View frg) {
         mBarChart_dieses_idr = frg.findViewById(R.id.frg_home_mpchart_dieses_idr);
 
-        mBarChart_sick_animal = frg.findViewById(R.id.frg_home_mpchart_sick_animal);
+        // mBarChart_sick_animal = frg.findViewById(R.id.frg_home_mpchart_sick_animal);
+
+
          barChart= (HorizontalBarChart) frg.findViewById(R.id.idr_barchart);
 
 
 
     }
 
-    private void showExampleGraph(View frg)
-    {
-        pieChart = frg.findViewById(R.id.activity_main_piechart);
+    private void showExampleGraph(View frg) {
+        // pieChart = frg.findViewById(R.id.activity_main_piechart);
+        mBarChart_sick_animal = frg.findViewById(R.id.frg_home_mpchart_sick_animal);
         setupPieChart();
         loadPieChartData();
     }
 
     private void setupPieChart() {
-        pieChart.setDrawHoleEnabled(false);
-        pieChart.setUsePercentValues(false);
-        pieChart.setEntryLabelTextSize(12);
-        pieChart.setEntryLabelColor(Color.BLACK);
+        mBarChart_sick_animal.setDrawHoleEnabled(false);
+        mBarChart_sick_animal.setUsePercentValues(false);
+        mBarChart_sick_animal.setEntryLabelTextSize(15);
+        mBarChart_sick_animal.setEntryLabelColor(Color.BLACK);
 //        pieChart.setCenterText("Spending by Category");
-        pieChart.setCenterTextSize(24);
-        pieChart.getDescription().setEnabled(false);
+        mBarChart_sick_animal.setCenterTextSize(20);
+        mBarChart_sick_animal.getDescription().setEnabled(false);
 
-        Legend l = pieChart.getLegend();
+        Legend l = mBarChart_sick_animal.getLegend();
         l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
         l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
         l.setOrientation(Legend.LegendOrientation.VERTICAL);
@@ -91,17 +97,18 @@ public class HomeFragment extends Fragment {
 
     private void loadPieChartData() {
         ArrayList<PieEntry> entries = new ArrayList<>();
-        entries.add(new PieEntry(37, "Food & Dining"));
-        entries.add(new PieEntry(4, "Medical"));
-        entries.add(new PieEntry(5, "Entertainment"));
+        entries.add(new PieEntry(15, "Camel"));
+        entries.add(new PieEntry(75, "Small Animal"));
+        entries.add(new PieEntry(250, "Large Animal"));
+        entries.add(new PieEntry(540, "Aquine"));
 
 
         ArrayList<Integer> colors = new ArrayList<>();
-        for (int color: ColorTemplate.MATERIAL_COLORS) {
+        for (int color : ColorTemplate.MATERIAL_COLORS) {
             colors.add(color);
         }
 
-        for (int color: ColorTemplate.VORDIPLOM_COLORS) {
+        for (int color : ColorTemplate.VORDIPLOM_COLORS) {
             colors.add(color);
         }
 
@@ -110,14 +117,14 @@ public class HomeFragment extends Fragment {
 
         PieData data = new PieData(dataSet);
         data.setDrawValues(true);
-        data.setValueFormatter(new IntegerFormatter(pieChart));
+        data.setValueFormatter(new IntegerFormatter(mBarChart_sick_animal));
         data.setValueTextSize(12f);
         data.setValueTextColor(Color.BLACK);
 
-        pieChart.setData(data);
-        pieChart.invalidate();
+        mBarChart_sick_animal.setData(data);
+        mBarChart_sick_animal.invalidate();
 
-        pieChart.animateY(1400, Easing.EaseInOutQuad);
+        mBarChart_sick_animal.animateY(1400, Easing.EaseInOutQuad);
     }
 //barchart
 //    private void showBarChart(View frg)
@@ -159,8 +166,6 @@ public class HomeFragment extends Fragment {
 //    }
 
 
-
-
     private void showIDR() {
         //Set the number of each share
         List<PieEntry> yvals = new ArrayList<>();
@@ -185,7 +190,6 @@ public class HomeFragment extends Fragment {
         lstPieValues.add(5);
 
 
-
         for (int i = 0; i < lstPieValues.size(); i++) {
             yvals.add(new PieEntry(lstPieValues.get(i)));
             if (i == 0) {
@@ -197,19 +201,17 @@ public class HomeFragment extends Fragment {
             } else if (i == 3) {
                 colors.add(getActivity().getResources().getColor(R.color.thm_yellow_graph_indicator));
             } else if (i == 4) {
-            colors.add(getActivity().getResources().getColor(R.color.thm_green_graph_indicator));
+                colors.add(getActivity().getResources().getColor(R.color.thm_green_graph_indicator));
             } else if (i == 5) {
-            colors.add(getActivity().getResources().getColor(R.color.thm_blue_app));
+                colors.add(getActivity().getResources().getColor(R.color.thm_blue_app));
             } else if (i == 6) {
-            colors.add(getActivity().getResources().getColor(R.color.app_blue_lightest));
+                colors.add(getActivity().getResources().getColor(R.color.app_blue_lightest));
             }
 
         }
 
 
-
-
-        PieChartManagger pieChartManagger = new PieChartManagger( mBarChart_dieses_idr , getContext());
+        PieChartManagger pieChartManagger = new PieChartManagger(mBarChart_dieses_idr, getContext());
         pieChartManagger.showSolidPieChartNew(yvals, colors);
     }
 
@@ -234,8 +236,6 @@ public class HomeFragment extends Fragment {
         lstPieValues.add(540);
 
 
-
-
         for (int i = 0; i < lstPieValues.size(); i++) {
             yvals.add(new PieEntry(lstPieValues.get(i)));
             if (i == 0) {
@@ -252,35 +252,62 @@ public class HomeFragment extends Fragment {
         }
 
 
-
-
-        PieChartManagger pieChartManagger = new PieChartManagger( mBarChart_sick_animal , getContext());
+        PieChartManagger pieChartManagger = new PieChartManagger(mBarChart_sick_animal, getContext());
         pieChartManagger.showSolidPieChartNew(yvals, colors);
     }
+
+
     private void SetData(int count, int range) {
 
         ArrayList<BarEntry> yVals = new ArrayList<>();
-        float barWidth = 9f;
+        float barWidth = 5;
         float spaceForBar = 10f;
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(1,40f ));
+        entries.add(new BarEntry(2,45f ));
+        entries.add(new BarEntry(3,30f ));
+
         for (int i = 0; i < count; i++) {
-            float val = (float) (Math.random() * range);
+            float val = (float) (Math.random() * count)+20;
+//            float val1 = (float) (Math.random() * count)+1;
+//            float val2 = (float) (Math.random() * count)+2;
+//            float val3 = (float) (Math.random() * count)+3;
             yVals.add(new BarEntry(i * spaceForBar, val));
+//            yVals.add(new BarEntry(i * spaceForBar, val1));
+//            yVals.add(new BarEntry(i * spaceForBar, val2));
+//            yVals.add(new BarEntry(i * spaceForBar, val3));
         }
+
             BarDataSet set1;
-            set1 = new BarDataSet(yVals, "Data Set1");
+            set1 = new BarDataSet(yVals, "Immediate Diseases Report");
+            set1.setDrawIcons(false);
+            set1.setStackLabels(new String[]{"L","S","M"});
+            set1.setColors(ColorTemplate.JOYFUL_COLORS);
             BarData data = new BarData(set1);
             barChart.setData(data);
+            data.setValueFormatter(new MyValueFormatter());
+
+            barChart.invalidate();
+            barChart.setFitBars(true);
+            barChart.getDescription().setEnabled(false);
+            barChart.setMaxVisibleValueCount(50);
+            barChart.setDrawValueAboveBar(true);
             data.setBarWidth(barWidth);
-
-      /////
-
+            barChart.setPinchZoom(false);
 
 
-
-
+    ///
 
 
     }
+
+
+
+
+
+
+
+
 
 
 
